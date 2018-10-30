9f9a80819593e7f834370ef8e0d3fa81225f2ced
ximitiejiang <ximitiejiang@163.com>
2018-10-31 07:50:17 +0800

    add basic template

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 23 15:01:21 2018
+
+@author: ubuntu
+"""
---
+++ utils/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 25 09:26:55 2018
+
+@author: ubuntu
+"""
+
